e64e51e36b4f7875fae785b983c9f4cd24a07a99
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-06-16 10:26:03 +0700

    add

---
+++ mxh/src/main/java/com/baconbao/mxh/MxhApplication.java
@@ -0,0 +1,14 @@
+package com.baconbao.mxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxhApplication.class, args);
+		
+	}
+
+}
